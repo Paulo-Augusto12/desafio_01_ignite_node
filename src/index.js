5dce11d69d5c8e2e82a1dd95ea564dbf1c0e1786
Paulo Augusto <paulo-augusto12@outlook.com>
2023-01-31 14:59:34 -0300

    feat: final commit. aplicação funcionando corretamente e com o funcionamento de todos os testes corretos

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ app.get("/todos", checksExistsUserAccount, (request, response) => {
   // Complete aqui
   const { user } = request;
 
-  return response.json(user.todos);
+  return response.send(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
@@ -70,10 +70,9 @@ app.post("/todos", checksExistsUserAccount, (request, response) => {
     deadline: new Date(deadline),
     created_at: new Date(),
   };
-
   user.todos.push(newTodo);
 
-  return response.status(201).end();
+  return response.status(201).send(newTodo);
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
@@ -86,12 +85,16 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
 
   const selectedTodo = user.todos.find((todoid) => todoid.id === id);
 
+  const checkId = user.todos.map((x) => x);
+
+  if (checkId.includes(selectedTodo) === false) {
+    return response.status(404).json({ error: "id não existente" });
+  }
+
   selectedTodo.title = title;
   selectedTodo.deadline = deadline;
 
-  console.log(selectedTodo);
-
-  return response.status(200).send();
+  return response.status(201).send(selectedTodo);
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
@@ -103,9 +106,17 @@ app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
 
   const selectedTodo = user.todos.find((todoid) => todoid.id === id);
 
+  const checkTodos = user.todos.map((x) => x);
+
+  if (checkTodos.includes(selectedTodo) === false) {
+    return response.status(404).json({
+      error: "should not be able to mark a non existing todo as done",
+    });
+  }
+
   selectedTodo.done = true;
 
-  return response.status(200).send();
+  return response.status(200).send(selectedTodo);
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
@@ -117,9 +128,17 @@ app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
 
   const selectedTodo = user.todos.find((todoid) => todoid.id === id);
 
+  const checkTodo = user.todos.map((x) => x);
+
+  if (checkTodo.includes(selectedTodo) === false) {
+    return response
+      .status(404)
+      .json({ error: "should not be able to delete a non existing todo" });
+  }
+
   user.todos.splice(selectedTodo, 1);
 
-  return response.status(200).send();
+  return response.status(204).send();
 });
 
 module.exports = app;
